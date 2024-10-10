63fa62e014d5827a65fe67fa2af24c55254a8a1a
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-10 14:23:24 +0200

    add function casual

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,13 @@ Pensate prima in italiano.
 Dividete in piccoli problemi la consegna.
 Individuate gli elementi di cui avete bisogno per realizzare il programma.
 Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array"*/
+
+//funzione per generare numeri random
+function random(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+let number_casual = []
+for (let i = 0; i < 5; i++) {
+	number_casual[i] = random(1, 100)
+}
+console.log(number_casual)
